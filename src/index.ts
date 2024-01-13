668c75e2a0f8b7fcedd1e6a487f659435c229e4b
mxmcg <maxmcgeedev@gmail.com>
2024-01-13 14:53:43 -0800

    Update port configuration for Heroku

--- src/index.ts
+++ src/index.ts
@@ -147,7 +147,7 @@ async function startServer() {
     // Start the Apollo Server
     const server = new ApolloServer({ typeDefs, resolvers });
     const { url } = await startStandaloneServer(server, {
-      listen: { port: process.env.PORT || 4000 },
+      listen: { port: Number(process.env.PORT) || 4000 },
     });
 
     console.log(`🚀 Server ready at: ${url}`);
